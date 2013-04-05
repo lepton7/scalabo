42fbcf3a38ec323a061b62cecd4de1d758f9db61
matsui <t_matsui@outlook.com>
2013-04-05 13:54:06 +0900

    例外発生時の動作を変更。 javaのtry-with-resourcesと同じように。

--- scalabo/src/main/scala/scalabo/util/ScopeGuard.scala
+++ scalabo/src/main/scala/scalabo/util/ScopeGuard.scala
@@ -4,74 +4,85 @@ object ExitType extends Enumeration {
   val EXIT, SUCCESS, FAILURE = Value
 }
 
-class Procedure(val exit: ExitType.Value, val proc: ()=>Any)
+class Procedure(exit: ExitType.Value, proc: =>Any) {
+  def call(throwed: Option[Throwable]): Option[Throwable] = {
+    try {
+      ((exit, throwed): @unchecked) match {
+        case (ExitType.EXIT,    _)       => proc
+        case (ExitType.SUCCESS, None)    => proc
+        case (ExitType.FAILURE, Some(e)) => proc
+      }
+      throwed
+    } catch {
+      case e: Throwable =>
+        throwed match {
+          case None => Some(e)
+          case Some(ex) =>
+            ex.addSuppressed(e)
+            Some(ex)
+        }
+    }
+  }
+}
 
 /**
  * D言語のスコープガード機能
- * 
+ *
  * サンプル：
  * {{{
  * ScopeGuard { scope =>
  *   val db = DB.open("some connection string")
  *   scope exit { db.close }
- *   
+ *
  *   val stmt = db.createStatement("update --- set ------")
  *   scope exit { stmt.close }
- *   
+ *
  *   scope failure { db.rollback }
+ *   scope success { db.commit }
  *   stmt.execute
- *   db.commmit
  * }
  * }}}
- * 
- * 上記のコードは以下とほぼ等価になる
+ *
+ * 上記のコードは以下とほぼ等価になる。
+ * 実際にはJava7のtry-with-resourcesと同じように、
+ * 複数例外が発生した場合に、後から発生した例外をsuppressedExceptionとして追加する処理が入っている。
+ * ただコードで書くと煩雑なので省略
  * {{{
  * val db = DB.open("some connection string")
- * 
+ *
  * try {
- *   val stmt = db.createStatement("update --- set ------")
- *   
+ * val stmt = db.createStatement("update --- set ------")
  *   try {
  *     stmt.execute
- *     
+ *
  *     db.commit
  *   } catch {
  *     case e: Throwable => {
  *       db.rollback
  *       throw e
  *     }
- *   }finally {
- *     try {
- *       stmt.close
- *     } catch {
- *       case ignored: Throwable =>
- *     }
+ *   } finally {
+ *     stmt.close
  *   }
  * } finally {
- *   try {
- *     db.close
- *   } catch {
- *     case ignored: Throwable =>
- *   }
+ *   db.close
  * }
  * }}}
  */
 object ScopeGuard {
   def apply(block: ScopeGuard => Any) = {
     val scope = new ScopeGuard
-    var exitType = ExitType.FAILURE
+    var throwed: Option[Throwable] = None
     try {
       block(scope)
-      exitType = ExitType.SUCCESS
+    } catch {
+      case e: Throwable => throwed = Some(e)
     } finally {
-      def safeCall(p: Procedure) {
-        try {
-          p.proc()
-        } catch {
-          case ignored: Throwable =>
-        }
-      }
-      scope.procs.reverseIterator.filter(p => p.exit == ExitType.EXIT || p.exit == exitType).foreach(safeCall)
+      throwed = scope.procs.reverseIterator.foldLeft(throwed)((t, p) => p.call(t))
+    }
+
+    (throwed: @unchecked) match {
+      case Some(e) => throw e
     }
   }
 }
@@ -83,14 +94,14 @@ class ScopeGuard private {
   private val procs = ArrayBuffer[Procedure]()
 
   def exit(proc: => Any): Unit = {
-    procs += new Procedure(ExitType.EXIT, () => proc)
+    procs += new Procedure(ExitType.EXIT, proc)
   }
 
   def success(proc: => Any): Unit = {
-    procs += new Procedure(ExitType.SUCCESS, () => proc)
+    procs += new Procedure(ExitType.SUCCESS, proc)
   }
 
   def failure(proc: => Any): Unit = {
-    procs += new Procedure(ExitType.FAILURE, () => proc)
+    procs += new Procedure(ExitType.FAILURE, proc)
   }
 }
